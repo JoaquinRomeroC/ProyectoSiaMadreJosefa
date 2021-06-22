75254780bc913d967052f61a5de321741c9b934c
JoaquinRomeroC <84754488+JoaquinRomeroC@users.noreply.github.com>
2021-06-22 11:52:06 -0400

    Entrega 6
    Login - Lista operadores

--- Proyecto (Conjunto de aplicaciones)/ProyectoSiaMadreJosefa/Aplicacion1/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from Aplicacion1.models import *
-
-admin.site.register(Operadores)
-admin.site.register(Patendidas)
-admin.site.register(Ficha)
